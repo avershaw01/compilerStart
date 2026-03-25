8e56d6065d75ae5042d46cb602e62c7bcdb55d5e
jojo <josephbarron1016@gmail.com>
2026-03-25 10:44:41 -0500

    Broke down scanFile into many functions.

--- src/LexAnalyzer.cpp
+++ src/LexAnalyzer.cpp
@@ -14,26 +14,104 @@
 // lexeme pairs i.e.  t_and and
 // Each pair appears on its own input line.
 // post: tokenmap has been populated - key: lexeme, value: token
+LexAnalyzer::LexAnalyzer(istream &infile) {
+    string token;
+    string lexeme;
+    while (infile >> token >> lexeme) {
+        tokenmap[lexeme] = token;
+    }
+}
 
 void LexAnalyzer::UpdateVectors(string lexeme, string token) {
     tokens.push_back(token);
     lexemes.push_back(lexeme);
 }
 
-void LexAnalyzer::PrintToOutput(ostream &outfile) const {
-
-}
-
-LexAnalyzer::LexAnalyzer(istream &infile) {
-    string token;
-    string lexeme;
-    while (infile >> token >> lexeme) {
-        // cout << lexeme << " -> " << token << endl; // my DEBUG
-        tokenmap[lexeme] = token;
+void LexAnalyzer::PrintToOutput(ostream &outfile,bool& errorFlag,string& errorMsg) const {
+    for (size_t i = 0; i < tokens.size(); i++) {
+        outfile << tokens[i] << " " << lexemes[i] << endl;
+        if (errorFlag) {
+            outfile << errorMsg << endl;
+        }
     }
 }
 
+void LexAnalyzer::CheckSymbols(int &i, string &line, string &lexeme, bool &errorFlag, string &errorMsg) {
+    if (i + 1 < line.length()) {
+        // End of File
+        string twoChars = lexeme + line[i + 1];
+        // twoChars variable is used to see if the pair is a valid symbol
 
+        if (tokenmap.count(twoChars)) {
+            lexeme = twoChars;
+            i++;
+        }
+    }
+    if (tokenmap.count(lexeme)) {
+        UpdateVectors(lexeme, tokenmap[lexeme]);
+    } else {
+        // This should only run if the compiler reaches an unknown symbol
+        // All other cases; letters, numbers, and valid symbols; should continue; (skips to next iteration)
+        cout << "ERROR -> Unknown Symbol" << endl;
+
+        // DEBUG to print to console what symbol broke the lexeme analyzer:
+        cout << "THE SYMBOL IN QUESTION: " << lexeme << endl;
+        errorFlag = true;
+        errorMsg = "THE SYMBOL IN QUESTION: " + lexeme;
+    }
+}
+
+void LexAnalyzer::CheckIDandKeyWords(int& i, string &line, string &lexeme, bool &errorFlag, string &errorMsg) {
+    char ch;
+    while (i < line.length() && ( isalnum(line[i+1]) || line[i+1] == '_')) {
+        ch = line[++i];
+        lexeme += ch;
+    }
+
+    if (tokenmap.contains(lexeme)) {
+        UpdateVectors(lexeme, tokenmap[lexeme]);
+    } else {
+        UpdateVectors(lexeme, "t_id");
+    }
+}
+void LexAnalyzer::CheckString(int& i, string &line, string &lexeme, bool &errorFlag, string &errorMsg, istream &infile) {
+    // checking for " midword
+    char ch;
+    if (i > 0 && isalnum(line[i-1])) {
+        errorFlag = true;
+        errorMsg = "UNEXPECTED QUOTATION: " + line;
+    }
+    // setting to non " so will pass while loop conditional
+    ch = '_';
+    bool complete = false;
+    // Only the data within the ""s is stored in the output file, "s are never added to the lexeme
+    i++;
+    while (ch != '"') {
+        // Continues until closing "
+        if (i < line.length())
+            ch = line[i];
+        else {
+            std::getline(infile, line);
+            i = 0;
+            ch = line[i];
+            i++;
+        }
+        lexeme += ch;
+        ch = line[++i];
+    }
+    if (ch == '"') {
+        UpdateVectors(lexeme, "t_text");
+        if (i+1 < line.length() && isalnum(line[i+1])) {
+            errorFlag = true;
+            errorMsg = "Unexpected character: " + line;
+        }
+    } else {
+        cout << "Failed to define STRING" << endl; // String is never closed
+        errorFlag = true;
+        errorMsg = "THE SYMBOL IN QUESTION: " + lexeme;
+    }
+
+}
 void LexAnalyzer::scanFile(istream &infile, ostream &outfile) {
     // While loops through input file source.txt
     string line;
@@ -44,7 +122,6 @@ void LexAnalyzer::scanFile(istream &infile, ostream &outfile) {
         int length = line.length();
         int i = -1;
         while ((++i < length) && !errorFlag) {
-
             // Instantiates temp variable to hold the lexeme
             string lexeme;
             char ch = line[i];
@@ -59,109 +136,27 @@ void LexAnalyzer::scanFile(istream &infile, ostream &outfile) {
                 while (i < line.length() && isdigit(line[i+1])) {
                     lexeme += line[++i];
                 }
-
-                // Adds to parallel vectors:
                 UpdateVectors(lexeme, "t_number");
-                continue;
             }
 
             // Now checking ALPHABET characters: (NOT a String, or else first char would be ")
             else if (isalpha(ch) || ch == '_') {
                 // Variables cannot start with a number
                 lexeme += ch;
-
-                // isalnum returns true if the char is a number OR letter
-                while (i < line.length() && ( isalnum(line[i+1]) || line[i+1] == '_')) {
-                    ch = line[++i];
-                    lexeme += ch;
-                }
-
-                if (tokenmap.contains(lexeme)) {
-                    UpdateVectors(lexeme, tokenmap[lexeme]);
-                } else {
-                    UpdateVectors(lexeme, "t_id");
-                }
-                continue;
+                CheckIDandKeyWords(i,line,lexeme,errorFlag,errorMsg);
             }
-
             // Checking for STRINGS (double quotes)
             else if (ch == '"') {
-                // checking for " midword
-                if (i > 0 && isalnum(line[i-1])) {
-                    errorFlag = true;
-                    errorMsg = "UNEXPECTED QUOTATION: " + line;
-                    continue;
-                }
-
-                // setting to non " so will pass while loop conditional
-                ch = '_';
-                bool complete = false;
-                // Only the data within the ""s is stored in the output file, "s are never added to the lexeme
-                i++;
-                while (ch != '"') {
-                    // Continues until closing "
-                    if (i < line.length())
-                        ch = line[i];
-                    else {
-                        std::getline(infile, line);
-                        i = 0;
-                        ch = line[i];
-                        i++;
-                    }
-                    lexeme += ch;
-                    ch = line[++i];
-                }
-                if (ch == '"') {
-                    UpdateVectors(lexeme, "t_text");
-                    // i++;
-                    if (i+1 < line.length() && isalnum(line[i+1])) {
-                        errorFlag = true;
-                        errorMsg = "Unexpected character: " + line;
-                    }
-                } else {
-                    cout << "Failed to define STRING" << endl; // String is never closed
-                    errorFlag = true;
-                    errorMsg = "THE SYMBOL IN QUESTION: " + lexeme;
-                }
-
+                CheckString(i,line,lexeme,errorFlag,errorMsg,infile);
             }
-
             // SYMBOLS
             else {
                 lexeme += ch;
-
-                if (i + 1 < line.length()) {
-                    // End of File
-                    string twoChars = lexeme + line[i + 1];
-                    // twoChars variable is used to see if the pair is a valid symbol
-
-                    if (tokenmap.count(twoChars)) {
-                        lexeme = twoChars;
-                        i++;
-                    }
-                }
-                if (tokenmap.count(lexeme)) {
-                    UpdateVectors(lexeme, tokenmap[lexeme]);
-                } else {
-                    // This should only run if the compiler reaches an unknown symbol
-                    // All other cases; letters, numbers, and valid symbols; should continue; (skips to next iteration)
-                    cout << "ERROR -> Unknown Symbol" << endl;
-
-                    // DEBUG to print to console what symbol broke the lexeme analyzer:
-                    cout << "THE SYMBOL IN QUESTION: " << lexeme << endl;
-                    errorFlag = true;
-                    errorMsg = "THE SYMBOL IN QUESTION: " + lexeme;
-
-                }
+                CheckSymbols(i, line, lexeme,errorFlag, errorMsg);
             }
         }
     }
     // If no errors ran return; pushes to output file what was read (using the parallel vectors)
-    for (size_t i = 0; i < tokens.size(); i++) {
-        outfile << tokens[i] << " " << lexemes[i] << endl;
-        if (errorFlag) {
-            outfile << errorMsg << endl;
-        }
-    }
+    PrintToOutput(outfile,errorFlag,errorMsg);
     cout << "It runs!" << endl; // DEBUG to console, ensures this section ran.
 }
--- src/LexAnalyzer.h
+++ src/LexAnalyzer.h
@@ -20,22 +20,12 @@ class LexAnalyzer {
 
         // other private methods
     void UpdateVectors(string lexeme, string token);
-    void PrintToOutput(ostream& outfile) const;
+    void PrintToOutput(ostream &outfile,bool& errorFlag,string& errorMsg) const;
+    void CheckSymbols(int &i, string &line, string &lexeme, bool &errorFlag, string &errorMsg);
+    void CheckIDandKeyWords(int& i, string &line, string &lexeme, bool &errorFlag, string &errorMsg);
+    void CheckString(int& i, string &line, string &lexeme, bool &errorFlag, string &errorMsg, istream &infile);
     public:
         LexAnalyzer(istream& infile);
-        // pre: parameter refers to open data file consisting of token and
-        // lexeme pairs i.e.  t_and and
-        // Each pair appears on its own input line.
-        // post: tokenmap has been populated - key: lexeme, value: token
-
         void scanFile(istream& infile, ostream& outfile);
-        // pre: 1st parameter refers to an open text file that contains source
-        // code in the language, 2nd parameter refers to an open empty output
-        // file
-        // post: If no error, the token and lexeme pairs for the given input
-        // file have been written to the output file (token lexeme).
-        // If there is an error, the incomplete token/lexeme pairs, as well as
-        // an error message have been written to the output file.
-        // A success or fail message has printed to the console.
 };
 #endif //LEXANALYZER_H
